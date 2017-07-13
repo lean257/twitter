91ec47785b78dc975a185baf887505ad05fc24f3
An Le <anle@ip-10-0-0-17.ec2.internal>
2017-07-13 00:50:44 -0400

    socket.io changes

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const nunjucks = require('nunjucks');
+const routes = require('./routes/');
+const bodyParser = require('body-parser');
+const socketio = require('socket.io');
+const volleyball = require('volleyball');
+
+var server = app.listen(port)
+var io = socketio.listen(server);
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {noCache: true});
+
+app.use('/', routes(io));
+app.use(function(req, res, next){
+  console.log(req.url);
+  next();
+});
+
+app.use(express.static('public'))
+
+app.use(volleyball)
+
+
---
+++ routes/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+const tweetBank = require('../tweetBank');
+
+module.exports = function(io) {
+  router.get('/', function (req, res) {
+    let tweets = tweetBank.list();
+    res.render( 'index', { tweets: tweets, showForm: true } );
+  });
+
+  router.get('/users/:name', function(req, res) {
+    let name = req.params.name;
+    let tweetsForUser = tweetBank.find( {name: name} );
+    res.render( 'index', { tweets: tweetsForUser } );
+  });
+
+  router.get('/tweets/:id', function(req, res) {
+    let id = Number(req.params.id);
+    let tweetByID = tweetBank.find( {id: id} );
+    res.render( 'index', { tweets: tweetByID } );
+  });
+
+  router.post('/tweets', function(req, res) {
+    var name = req.body.name;
+    var text = req.body.text;
+    tweetBank.add(name, text);
+    res.redirect('/');
+  });
+
+  return router;
+}
